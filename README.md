e41e19a591e896d5c28ecf31120e2b4d21f12c6f
Ivan Atias <ivan.d.atias@gmail.com>
2023-06-28 04:13:39 -0400

    📦 feat: add image downloader functionality

---
+++ README.md
@@ -0,0 +1,30 @@
+# Image Download Util
+
+CLI tool built using Go to download images from a `.txt` file containing all the images addresses. 
+
+## Installation
+
+To install, follow these steps:
+
+- Clone the repository: git clone https://github.com/ivanatias/imgdl.git
+- Navigate to the cloned directory: cd imgdl
+- Install the tool using Go: `go install`
+
+### Important: 
+
+You must install Go in your system. To install, follow the instructions on [Go's website](https://go.dev/doc/install).
+
+## Usage 
+
+Once installed, you can use the tool as follows: 
+
+- Unix-like systems: `./imgdl -from /path/to/text.txt`
+- Windows: `imgdl.exe -from C:\path\to\text.txt`
+
+Optionally, you can provide a path to indicate where all the images should be saved. By default, all images will be saved on a folder called `imgdl` on the current working directory from where the CLI has been executed.
+
+Providing a path where images should be saved:
+
+`./imgdl -from /path/to/text.txt -to /path/where/images/are/saved`
+
+Note that while the `to` flag is optional, `from` is required. 
--- main.go
+++ main.go
@@ -1,7 +1,110 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+var (
+	green  = color.New(color.FgGreen)
+	yellow = color.New(color.FgYellow)
+	cyan   = color.New(color.FgCyan)
+)
 
 func main() {
-	fmt.Println("Init")
+	var from string
+	var to string
+
+	cwd, err := os.Getwd()
+
+	if err != nil {
+		panic(err)
+	}
+
+	defaultTo := filepath.Join(cwd, "imgdl")
+
+	flag.StringVar(&from, "from", "", "Path to text file with all images urls")
+	flag.StringVar(
+		&to,
+		"to",
+		defaultTo,
+		"Path to folder where images will be saved",
+	)
+	flag.Parse()
+
+	if len(from) == 0 {
+		panic("Path to text file with all images urls is required")
+	}
+
+	fromSlice := strings.Split(from, "/")
+	textFile := fromSlice[len(fromSlice)-1]
+	ext := strings.Split(textFile, ".")[1]
+
+	if ext != "txt" {
+		panic("Path of file with all images urls must have .txt extension")
+	}
+
+	file, err := os.Open(from)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		imageUrl := scanner.Text()
+		response, err := http.Get(imageUrl)
+
+		if err != nil {
+			panic(err)
+		}
+
+		defer response.Body.Close()
+
+		contentType := response.Header.Get("Content-Type")
+		contentSlice := strings.Split(contentType, "/")
+
+		if contentSlice[0] != "image" {
+			yellow.Printf(
+				"Skipping resource %s because it's not an image\n",
+				imageUrl,
+			)
+
+			continue
+		}
+
+		imageData, err := io.ReadAll(response.Body)
+
+		if err != nil {
+			panic(err)
+		}
+
+		imageFilename := filepath.Base(imageUrl)
+
+		err = os.MkdirAll(to, 0755)
+
+		if err != nil {
+			panic(err)
+		}
+
+		err = os.WriteFile(filepath.Join(to, imageFilename), imageData, 0644)
+
+		if err != nil {
+			panic(err)
+		}
+
+		cyan.Printf("Saved %s\n", imageFilename)
+	}
+
+	green.Printf("\nAll images saved on %s\n", to)
 }
